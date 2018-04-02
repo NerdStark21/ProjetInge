ff9c428704bf78bad0dbadbcc3c41a7c4d689f3b
Aurelien Turpin <aurelien.turpin@telecom-st-etienne.fr>
2018-04-02 20:07:52 +0200

    Ersatz de squelette du site

---
+++ Code/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "link.php" ?>
+</head>
+	<?php include "skeleton2.php" ?>
+</html>
---
+++ Code/skeleton.php
@@ -0,0 +1,66 @@
+<nav>
+    <div class="right_col" role="main" style="min-height: 552px;">
+        <div class="col-md-12 col-sm-12 col-xs-12"></div>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Mes applications <small>1</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>Nom de l'application</th>
+                            <th>Etat</th>
+                            <th>Date de création</th>
+                            <th>Nombre d'api</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td><a href="/developpeur/application/liste#collapsec05dbda9-acfa-4939-bdba-a5805229a166" class="underline">Projet ingé</a></td>
+                            <td><i class="fa fa-check fa-color-green"></i></td>
+                            <td>15/03/2018 14:05</td>
+                            <td>3 Api(s)</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div class="clearfix"></div>
+                    <div class="col-xs-12 text-right">
+                        <a href="/developpeur/application/liste">Voir toutes mes applications</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="mentions" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog ">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                        </button>
+                        <h4 class="modal-title" id="myModalLabel">Mentions légales</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>SUEZ ou ses filiales et sociétés affiliées (ci-après « SUEZ Smart Solutions ») offrent aux utilisateurs l’accès au présent site (ci-après « le Site »), sur la base des conditions qui suivent, pour le compte de la métropole de Saint-Etienne. L’utilisation du Site vaut acceptation de ces conditions, les utilisateurs s’engagent à s’y conformer et à les respecter.</p>
+
+                        <p><strong>EDITEUR DU SITE</strong><br>
+                            Le site https:/data.saint-etienne-metropole.fr est édité par SMART SOLUTIONS, société SAS, au capital de 100 000 &nbsp;Euros, dont le siège social est situé 38 rue du Président Wilson – 78230 LE PECQ, immatriculée au Registre du Commerce et des Sociétés de VERSAILLES sous le numéro 509 561 395.</p>
+
+                        <p><strong>Représentant légal :</strong> Farrokh FOTOOHI&nbsp;<br>
+                            <strong>Directeur de la publication :</strong> Francis CAMPAN&nbsp;<br>
+                            <strong>Directeur de la rédaction :</strong> Francis CAMPAN</p>
+
+                        <p><strong>HEBERGEUR DU SITE</strong><br>
+                            Le site est hébergé par la société MICROSOFT sur son cloud AZURE WEST EUROPE :&nbsp;
+                            39 Quai du Président Roosevelt, 92130 Issy-les-Moulineaux</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
