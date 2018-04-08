896ac60e7c6136f67d8f63a749758e484935bfea
Aurelien Turpin <aurelien.turpin@telecom-st-etienne.fr>
2018-04-08 21:23:21 +0200

    L'historique de consommation

---
+++ Ping/assets/js/historique.js
@@ -0,0 +1,81 @@
+
+let listMonth = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+let listEnergy = {"water" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                  "electricity" : [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+                  "gas" : [25, 26, 27, 28, 32, 30, 31, 32, 33, 34, 35, 36]};
+
+let listCompare = {"water" : [1, 2, 3, 3, 5, 6, 7, 8, 9, 10, 11, 12],
+                  "electricity" : [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+                  "gas" : [31, 15, 27, 29, 29, 30, 31, 40, 33, 34, 35, 37]};
+
+$(document).ready(function (){
+// Pour afficher tous les mois dans le tableau
+for(let k=0; k<12;k++){
+    let clone = $(".monthClone").clone();
+    clone.removeClass("monthClone");
+    clone.text(listMonth[k]);
+    $(".ligne_header").append(clone);
+}
+
+function remplir(energy, listEnergy, listCompare, année){
+    for(k=0;k<12;k++){
+        let clone = $(".".concat(energy)).find(".valueClone").clone();
+        clone.removeClass("valueClone");
+        let actualEnergy = listEnergy[energy][k];
+        let previousEnergy = listCompare[energy][k];
+        console.log("Energy");
+        console.log(actualEnergy);
+        console.log(1.2*actualEnergy);
+        console.log(previousEnergy);
+        if(actualEnergy <= 1.05*previousEnergy){
+            clone.addClass("green");
+        }
+        else if(actualEnergy <= 1.15*previousEnergy){
+            clone.addClass("orange");
+        }
+        else{
+            clone.addClass("red");
+        }
+        clone.text(listEnergy[energy][k]);
+        $(".".concat(energy)).append(clone);
+    }
+    console.log("FIN");
+};
+
+let energyType = ["water", "electricity", "gas"];
+for (j=0;j<3;j++){
+    console.log(energyType[j], listEnergy, listCompare);
+    console.log(j);
+    remplir(energyType[j]);
+}
+})
+
+/*
+//Pour faire le OUVRIR / FERMER du tableau
+function fermer(){
+    $("section").find("th").first().text("OUVRIR");
+    $('tbody').hide();
+}
+
+function ouvert(){
+    $("section").find("th").first().text("FERMER");
+    $('tbody').show();
+}
+
+$("#fermer").click(function(event){
+    if($("section").find("th").first().text() == "FERMER")
+    fermer();
+    else
+    ouvert();
+});
+
+$("input").keypress(function(event){
+    str=$(this).val();
+});
+
+function _changeTheme(color){
+    let ligne_color = $("th");
+    ligne_color.removeClass("grey blue red green");
+    ligne_color.addClass(color);
+};
+*/
---
+++ Ping/index_nettoye.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html><head>
+	<title>Editorial by HTML5 UP</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<!-- CSS -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="assets/css/historique.css">
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="assets/js/historique.js"></script>
+</head>
+	<body class="">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="index.html" class="logo"><strong>Editorial</strong> by HTML5 UP</a>
+									<ul class="icons">
+										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon fa-medium"><span class="label">Medium</span></a></li>
+									</ul>
+								</header>
+
+							<!-- Banner -->
+								<section id="banner">
+									<div class="content">
+										<header>
+											<h1>Hi, I’m Editorial by HTML5 UP</h1>
+											
+										</header>
+										
+										
+									</div>
+									
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Historique de consommation à l'année (2018)</h2>
+									</header>
+									<div class="historique">
+										<article>
+											<table>
+    											<thead>
+            										<tr class="ligne_header">
+            											<th class="grey h_nom">Type d'énergie<span></span></th>
+                										<th class="monthClone">Mois<span></span></th>
+            										</tr>
+        										</thead>
+        										<tbody>
+        											<tr class="electricity">
+            											<td class="energyType">Electricité</td>
+                										<td class="valueClone"></td>
+           											</tr>
+           											<tr class="water">
+            											<td class="energyType">Eau</td>
+                										<td class="valueClone"></td>
+           											</tr>
+           											<tr class="gas">
+            											<td class="energyType">Gaz</td>
+                										<td class="valueClone"></td>
+           											</tr>
+        										</tbody>
+											</table>
+										</article>
+									</div>
+								</section>
+
+							<!-- Section -->
+								
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar" class="inactive">
+						<div class="inner">
+
+							<!-- Search -->
+								<section id="search" class="alt">
+									<form method="post" action="#">
+										<input type="text" name="query" id="query" placeholder="Search">
+									</form>
+								</section>
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="index.html">Homepage</a></li>
+										<li><a href="generic.html">Generic</a></li>
+										<li><a href="elements.html">Elements</a></li>
+										<li>
+											<span class="opener">Submenu</span>
+											<ul>
+												<li><a href="#">Lorem Dolor</a></li>
+												<li><a href="#">Ipsum Adipiscing</a></li>
+												<li><a href="#">Tempus Magna</a></li>
+												<li><a href="#">Feugiat Veroeros</a></li>
+											</ul>
+										</li>
+									</ul>
+								</nav>
+
+							
+								
+
+							
+								<section>
+									<header class="major">
+										<h2>Get in touch</h2>
+									</header>
+									<p>Peut-être qqc ici</p>
+									<ul class="contact">
+										<li class="fa-envelope-o"><a href="#">information@untitled.tld</a></li>
+										<li class="fa-phone">(000) 000-0000</li>
+										<li class="fa-home">1234 Somewhere Road #8254<br>
+										Nashville, TN 00000-0000</li>
+									</ul>
+								</section>
+
+							<!-- Footer -->
+								<footer id="footer">
+									<p class="copyright">© Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+    <p> Il ne faudra pas oublier de mentionner les licences ici !</p>
+								</footer>
+
+						</div>
+					<a href="#sidebar" class="toggle">Toggle</a></div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	
+</body></html>
